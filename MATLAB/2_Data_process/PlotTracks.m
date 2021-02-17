936fc94fbc48c0977837cdbcb8e0dacc2dc4fb7e
Mikhail P. Tokarev <52541332+mikhailpt@users.noreply.github.com>
2021-02-17 19:34:48 +0700

    Delete MATLAB directory

--- MATLAB/2_Data_process/PlotTracks.m
+++
@@ -1,7 +0,0 @@
-function PlotTracks(tracks, fig, marker)
-if ~exist('fig', 'var') fig = 1;  end
-if ~exist('marker', 'var') marker = 'b.';  end
-figure(fig);
-plot3(tracks(:, 3), tracks(:, 4), tracks(:, 5), marker);
-end
-
